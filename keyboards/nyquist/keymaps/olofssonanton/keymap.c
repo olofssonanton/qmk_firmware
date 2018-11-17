c4e272bc2d1a1c0abf793ed523b9503c8e7458cd
Anton Olofsson <olofssonanton93@gmail.com>
2018-11-17 21:42:00 +0100

    Re-add left bracket to preserve compatibility with Nordic layout

--- keyboards/nyquist/keymaps/olofssonanton/keymap.c
+++ keyboards/nyquist/keymaps/olofssonanton/keymap.c
@@ -24,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * .-----------------------------------------------------------------------------------------------------------.
  * | `      | 1      | 2      | 3      | 4      | 5      | 6      | 7      | 8      | 9      | 0      |        |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------|
- * | ESC    | Q      | W      | E      | R      | T      | Y      | U      | I      | O      | P      |        |
+ * | ESC    | Q      | W      | E      | R      | T      | Y      | U      | I      | O      | P      | [      |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------+--------|
  * | TAB    | A      | S      | D      | F      | G      | H      | J      | K      | L      | ;      | '      |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------|
@@ -35,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
  [_QW] = LAYOUT( \
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    ___X___,  \
-  KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    ___X___,  \
+  KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,  \
   KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,  \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,  \
   KC_LCTL, KC_LGUI, ___X___, KC_LALT, LW,      KC_SPC,  KC_ENT,  KC_BSPC, RS,      KC_RALT, KC_APP,  KC_RCTL   \
