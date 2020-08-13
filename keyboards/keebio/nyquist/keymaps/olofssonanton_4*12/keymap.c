70add6239e11b1d0f35aa09cf77fa6620b9fb295
Anton Olofsson <me@antonolofsson.com>
2020-08-13 22:49:05 +0200

    Getting experimental with mouse layers

--- keyboards/keebio/nyquist/keymaps/olofssonanton_4*12/keymap.c
+++ keyboards/keebio/nyquist/keymaps/olofssonanton_4*12/keymap.c
@@ -8,13 +8,18 @@ extern keymap_config_t keymap_config;
 #define _NUM 3
 #define _SYM 4
 #define _FKEY 5
+#define _MOUS 7
+#define _SCRL 8
 
 enum custom_keycodes {
   NAV = SAFE_RANGE,
+  MOU,
   FUN,
   NUM,
   SYM,
-  FKEY
+  FKEY,
+  MOUS,
+  SCRL,
 };
 
 #define _______ KC_TRNS
@@ -41,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_NAV] = LAYOUT_ortho_4x12(
-    _______, FKEY,    ___X___, ___X___, SYM,     ___X___, ___X___, KC_HOME, KC_UP,   KC_END,  KC_PGUP, ___X___,
+    _______, FKEY,    ___X___, MOUS,    SYM,     ___X___, ___X___, KC_HOME, KC_UP,   KC_END,  KC_PGUP, ___X___,
     _______, KC_LCTL, KC_LSFT, KC_LGUI, KC_LALT, ___X___, ___X___, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, ___X___,
     _______, NUM,     ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, _______,
     _______, _______, _______, _______, _______, _______, _______, KC_DEL,  ___X___, _______, _______, _______
@@ -73,6 +78,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ___X___, _______, _______, _______, _______, ___X___, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,
     ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11 , KC_F12,
     ___X___, ___X___, ___X___, ___X___, _______, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___
+  ),
+
+  [_MOUS] = LAYOUT_ortho_4x12(
+    ___X___, ___X___, SCRL,    _______, KC_ACL1, ___X___, ___X___, ___X___, KC_MS_U, ___X___, ___X___, ___X___,
+    ___X___, _______, _______, _______, _______, ___X___, ___X___, KC_MS_L, KC_MS_D, KC_MS_R, ___X___, ___X___,
+    ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___,
+    ___X___, ___X___, ___X___, ___X___, _______, ___X___, KC_BTN1, KC_BTN2, ___X___, ___X___, ___X___, ___X___
+  ),
+
+  [_SCRL] = LAYOUT_ortho_4x12(
+    ___X___, ___X___, _______, _______, _______, ___X___, ___X___, ___X___, KC_WH_U, ___X___, ___X___, ___X___,
+    ___X___, _______, _______, _______, _______, ___X___, ___X___, KC_WH_L, KC_WH_D, KC_WH_R, ___X___, ___X___,
+    ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___, ___X___,
+    ___X___, ___X___, ___X___, ___X___, _______, ___X___, KC_BTN3, ___X___, ___X___, ___X___, ___X___, ___X___
   )
 
 /*
@@ -124,6 +143,22 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         layer_on(_FKEY);
       } else {
         layer_off(_FKEY);
+      }
+      return false;
+      break;
+    case MOUS:
+      if (record->event.pressed) {
+        layer_on(_MOUS);
+      } else {
+        layer_off(_MOUS);
+      }
+      return false;
+      break;
+    case SCRL:
+      if (record->event.pressed) {
+        layer_on(_SCRL);
+      } else {
+        layer_off(_SCRL);
       }
       return false;
       break;
